9f96c7515974557f3499cb33d6f0e403ee1ec359
Marcel <seasky.mlc@gmail.com>
2020-02-20 22:45:11 +0100

    node_modules fixed

--- src/containers/App.js
+++ src/containers/App.js
@@ -4,6 +4,7 @@ import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
 
+//You need to extend component and call super() in order to work with stats
 class App extends Component  {
 	constructor() {
 		super()
